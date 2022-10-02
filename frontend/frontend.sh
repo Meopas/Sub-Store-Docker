dac145b2d9ab6976c088fd22f0aaf354d60d460f
SaintWe <93098501+SaintWe@users.noreply.github.com>
2022-10-02 14:57:01 +0800

    +1s

--- frontend/frontend.sh
+++ frontend/frontend.sh
@@ -2,9 +2,9 @@
 
 echo -e "======================== 1. 启动nginx ========================\n"
 
-    if [ ! -f "/etc/nginx/conf.d/front.conf" ]; then
+    if [ ! -f "/etc/nginx/conf.d/frontend.conf" ]; then
         echo -e "生成 nginx 配置文件\n"
-        envsubst '${ALLOW_IP}' < /etc/nginx/conf.d/front.template > /etc/nginx/conf.d/front.conf    
+        envsubst '${ALLOW_IP}' < /etc/nginx/conf.d/frontend.template > /etc/nginx/conf.d/frontend.conf    
     fi
     nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
 
@@ -12,7 +12,7 @@ echo -e "==============================================================\n"
 
 echo -e "======================== 2、启动 Sub-Store 界面 ========================\n"
 
-    sed -i "s|https://sub.store|${DOMAIN}|g" grep "https://sub.store" -rl /Sub-Store/Front/dist
+    sed -i "s|https://sub.store|${DOMAIN}|g" grep "https://sub.store" -rl /Sub-Store/frontend/dist
 
 echo -e "==============================================================\n"
 
